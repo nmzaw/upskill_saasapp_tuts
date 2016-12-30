f40d5d404844cea026cc7d3b7de18d912cd822da
nmzaw <pico83@gmail.com>
2016-12-30 08:27:38 +0000

    Added contact form mail view and ruby mail deliver function

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,6 +6,10 @@ class ContactsController < ApplicationController
     def create
         @contact = Contact.create(contact_params)
         if @contact.save
+            name = params[:contact][:name]
+            email = params[:contact][:email]
+            body = params[:contact][:comments]
+            ContactMailer.contact_email(name,email,body).deliver
             flash[:success] = "Your message has been sent."
             redirect_to new_contact_path
         else
